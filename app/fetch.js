07d05746adcbf44e371a5fa8a2bb3429ad29d11d
alexisb1994 <alexis__blanco@hotmail.com>
2024-06-06 20:32:17 -0300

    add:image+name.json

--- app/fetch.js
+++ app/fetch.js
@@ -1,9 +1,27 @@
+const $main=document.querySelector("main");
+
+
 fetch("https://hp-api.onrender.com/api/characters")
-.then((respuesta)=>{
-   console.log(respuesta); 
-   return respuesta.json();})
-.then((data)=>{
-    // console.log(data);
-});
+ .then(response => response.json())
+    .then(data => {
+        data.forEach(personaje => {
+            console.log(personaje);
+$main.innerHTML+=`
+<div class="character">
+<img src="${personaje.image}" alt="imagen de ${personaje.name}">
+<h2>${personaje.name}</h2>
+<h2>Nombre: ${personaje.name}</h2>
+<h3>Especie: ${personaje.species}</h3>
+
+</div>
+`
+
+
+        });
+    });
+    
+//  );
+
+// });
 
 
