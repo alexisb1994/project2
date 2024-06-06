97ce62f039db9ad46990a40098325320309d9d2b
alexisb1994 <alexis__blanco@hotmail.com>
2024-06-06 19:13:26 -0300

    add:Project Start

---
+++ app/fetch.js
@@ -0,0 +1,9 @@
+fetch("https://hp-api.onrender.com/api/characters")
+.then((respuesta)=>{
+   console.log(respuesta); 
+   return respuesta.json();})
+.then((data)=>{
+    // console.log(data);
+});
+
+
