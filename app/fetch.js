fea88b6de1d6812542d0c81a9d04285e244ffbd7
alexisb1994 <alexis__blanco@hotmail.com>
2024-06-09 23:40:07 -0300

    add:books-galery

--- app/fetch.js
+++ app/fetch.js
@@ -3,50 +3,57 @@ const $characters = document.querySelector("section.characters");
 fetch("https://hp-api.onrender.com/api/characters")
   .then(respuesta => respuesta.json())
   .then(data => {
-  
+
     const mostrarPersonajes = (personajes) => {
       $characters.innerHTML = ""; 
 
       personajes.forEach(personaje => {
+        const actorEtiqueta = personaje.gender === "male" ? "Actor" : "Actriz";
+        const nameForSearch = personaje.name.replace(/ /g, '+');
+
         const characterHTML = `
           <div class="character">
-          <div class="character-img"> 
-          <img src="${personaje.image}" alt="imagen de ${personaje.name}">
-          </div>  
-          <div class="character-title">
-              <h2>${personaje.name}</h2>
-              <div class="character-profile">
-                <div>
-                  <h3>Nombre:</h3>
-                  <p>${personaje.name}</p>
-                </div>
-                <div>
-                  <h3>Actor:</h3>
-                  <p>${personaje.actor}</p>
-                </div>
-                <div>
-                  <h3>Año de nacimiento:</h3>
-                  <p>${personaje.yearOfBirth}</p>
-                </div>
-                <div>
-                  <h3>Sexo:</h3>
-                  <p>${personaje.gender}</p>
-                </div>
-                <div>
-                  <h3>Especie:</h3>
-                  <p>${personaje.species}</p>
+          <div class="character-link">
+            <a href="https://www.google.com/search?q=${nameForSearch}&oq=harr&gs_lcrp=EgZjaHJvbWUqDwgAEEUYOxiDARixAxiABDIPCAAQRRg7GIMBGLEDGIAEMgYIARBFGDkyCggCEAAYsQMYgAQyDQgDEAAYgwEYsQMYgAQyCggEEAAYsQMYgAQyBggFEEUYPDIGCAYQRRg9MgYIBxBFGD0yBggIEEUYQdIBCDEzNDFqMGo0qAIAsAIB&sourceid=chrome&ie=UTF-8" target="_blank" class="character-link">
+            </div>
+              <div class="character-img"> 
+                <img src="${personaje.image}" alt="imagen de ${personaje.name}">
+              </div>  
+              <div class="character-title">
+                <h2>${personaje.name}</h2>
+                <div class="character-profile">
+                  <div>
+                    <h3>Nombre:</h3>
+                    <p>${personaje.name}</p>
+                  </div>
+                  <div>
+                    <h3>${actorEtiqueta}:</h3>
+                    <p>${personaje.actor}</p>
+                  </div>
+                  <div>
+                    <h3>Año de nacimiento:</h3>
+                    <p>${personaje.yearOfBirth}</p>
+                  </div>
+                  <div>
+                    <h3>Sexo:</h3>
+                    <p>${personaje.gender}</p>
+                  </div>
+                  <div>
+                    <h3>Especie:</h3>
+                    <p>${personaje.species}</p>
+                  </div>
                 </div>
               </div>
-            </div>
+            </a>
           </div>
         `;
-        $characters.innerHTML += characterHTML; // 
+        $characters.innerHTML += characterHTML;
       });
     };
 
-        mostrarPersonajes(data);
+    mostrarPersonajes(data);
 
-       const $inpDePersonajes = document.getElementById("inpDePersonajes");
+    const $inpDePersonajes = document.getElementById("inpDePersonajes");
     $inpDePersonajes.addEventListener("input", () => {
       const searchTerm = $inpDePersonajes.value.toLowerCase();
       const filteredCharacters = data.filter((personaje) =>
@@ -55,4 +62,6 @@ fetch("https://hp-api.onrender.com/api/characters")
       mostrarPersonajes(filteredCharacters);
     });
   })
-  
+  .catch(error => {
+    console.error("Hubo un problema con la solicitud fetch:", error);
+  });
