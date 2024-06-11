efcaeae1ac170672112773ce469aeef469f4edc3
alexisb1994 <alexis__blanco@hotmail.com>
2024-06-11 01:09:37 -0300

    add:responsive-design-characters

--- app/fetch.js
+++ app/fetch.js
@@ -12,40 +12,38 @@ fetch("https://hp-api.onrender.com/api/characters")
         const nameForSearch = personaje.name.replace(/ /g, '+');
 
         const characterHTML = `
-          <div class="character">
-          <div class="character-link">
-            <a href="https://www.google.com/search?q=${nameForSearch}&oq=harr&gs_lcrp=EgZjaHJvbWUqDwgAEEUYOxiDARixAxiABDIPCAAQRRg7GIMBGLEDGIAEMgYIARBFGDkyCggCEAAYsQMYgAQyDQgDEAAYgwEYsQMYgAQyCggEEAAYsQMYgAQyBggFEEUYPDIGCAYQRRg9MgYIBxBFGD0yBggIEEUYQdIBCDEzNDFqMGo0qAIAsAIB&sourceid=chrome&ie=UTF-8" target="_blank" class="character-link">
-            </div>
-              <div class="character-img"> 
-                <img src="${personaje.image}" alt="imagen de ${personaje.name}">
-              </div>  
-              <div class="character-title">
-                <h2>${personaje.name}</h2>
-                <div class="character-profile">
-                  <div>
-                    <h3>Nombre:</h3>
-                    <p>${personaje.name}</p>
-                  </div>
-                  <div>
-                    <h3>${actorEtiqueta}:</h3>
-                    <p>${personaje.actor}</p>
-                  </div>
-                  <div>
-                    <h3>Año de nacimiento:</h3>
-                    <p>${personaje.yearOfBirth}</p>
-                  </div>
-                  <div>
-                    <h3>Sexo:</h3>
-                    <p>${personaje.gender}</p>
-                  </div>
-                  <div>
-                    <h3>Especie:</h3>
-                    <p>${personaje.species}</p>
-                  </div>
-                </div>
+        <div class="character">
+        <a href="https://www.google.com/search?q=${nameForSearch}&oq=harr&gs_lcrp=EgZjaHJvbWUqDwgAEEUYOxiDARixAxiABDIPCAAQRRg7GIMBGLEDGIAEMgYIARBFGDkyCggCEAAYsQMYgAQyDQgDEAAYgwEYsQMYgAQyCggEEAAYsQMYgAQyBggFEEUYPDIGCAYQRRg9MgYIBxBFGD0yBggIEEUYQdIBCDEzNDFqMGo0qAIAsAIB&sourceid=chrome&ie=UTF-8" target="_blank" class="character-link">
+          <div class="character-img"> 
+            <img src="${personaje.image}" alt="imagen de ${personaje.name}">
+          </div>  
+          <div class="character-title">
+            <h2>${personaje.name}</h2>
+            <div class="character-profile">
+              <div class="profile">
+                <h3>Nombre:</h3>
+                <p>${personaje.name}</p>
               </div>
-            </a>
+              <div>
+                <h3>${actorEtiqueta}:</h3>
+                <p>${personaje.actor}</p>
+              </div>
+              <div>
+                <h3>Año de nacimiento:</h3>
+                <p>${personaje.yearOfBirth}</p>
+              </div>
+              <div>
+                <h3>Sexo:</h3>
+                <p>${personaje.gender}</p>
+              </div>
+              <div>
+                <h3>Especie:</h3>
+                <p>${personaje.species}</p>
+              </div>
+            </div>
           </div>
+        </a>
+      </div>
         `;
         $characters.innerHTML += characterHTML;
       });
